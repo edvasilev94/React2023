26761ce4bc94ece102c91edd3910f35c902212a4
edvasilev94 <94469360+edvasilev94@users.noreply.github.com>
2023-12-04 17:32:18 +0200

    edit hero section description

--- vite-project/src/components/HeroSection/HeroSection.jsx
+++ vite-project/src/components/HeroSection/HeroSection.jsx
@@ -6,7 +6,7 @@ export default function HeroSection () {
                     <div className="row justify-content-center py-5">
                         <div className="col-lg-10 pt-lg-5 mt-lg-5 text-center">
                             <h1 className="display-3 text-white mb-3 animated slideInDown">Enjoy The Best Mountain Tracks</h1>
-                            <p className="fs-4 text-white mb-4 animated slideInDown">Tempor erat elitr rebum at clita diam amet diam et eos erat ipsum lorem sit</p>
+                            <p className="fs-4 text-white mb-4 animated slideInDown">In this website you can find the best tracks in Bulgaria and all around the world.</p>
                             <div className="position-relative w-75 mx-auto animated slideInDown">
                             </div>
                         </div>
